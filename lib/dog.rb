eef55ce792d23b22b3c9649cac7798869d66d1f6
Matthew Rhone <kritoke@gmail.com>
2017-02-13 12:03:56 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
   # code goes here
+  attr_reader :mood, :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name
+    @name.dup.freeze
+  end
+
+  def mood=(new_mood)
+    @mood = new_mood
+  end
+
 end
