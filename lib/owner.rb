e740bde81b2143c970c0c3645c201af8ee875eba
Panagiotis Skarlas <pskarl@gmail.com>
2017-03-10 18:54:56 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,7 +40,38 @@ class Owner
   end
 
   def walk_dogs
-    self.pets[:dogs]
+    self.pets[:dogs].map do |x|
+      x.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    self.pets[:cats].map do |x|
+      x.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    self.pets[:fishes].collect do |x|
+      x.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    self.pets.map do |key, value|
+      self.pets[key].map do |value|
+        value.mood = "nervous"
+      end
+      self.pets[key].clear
+    end
+  end
+
+  def list_pets
+    counter = {}
+    self.pets.each do |key, value|
+        counter[key] = self.pets[key].count
+    end
+    "I have #{counter[:fishes]} fish, #{counter[:dogs]} dog(s), and #{counter[:cats]} cat(s)."
   end
 
 end
