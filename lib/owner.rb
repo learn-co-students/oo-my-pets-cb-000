58c8937bd0a00406b1336bf5e916361e59becadd
Gabriel Constantin Staicu <gabriel_staicu@yahoo.com>
2017-05-17 12:03:45 +0000

    work im progress

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
   # code goes here
+  attr_accessor :name
+  @pets = {fishes: [], cats: [], dogs: []}
+  @@owners = []
+
+  def initialize(name)
+    @name = name
+    @@owners << self
+  end
+
+  def self.reset_all
+    @@owners = []
+  end
+
+  def self.count
+    @@owners.size
+  end
+
+  def self.all
+    @@owners
+  end
+
 end
