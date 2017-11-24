408fc16afef2386e143089e18122efd6406635cf
Mark Fowler <fowlermark488@gmail.com>
2017-11-24 06:03:34 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,66 +1,65 @@
 class Owner
-  # code goes here
-  attr_accessor :pets, :name
+
+  OWNERS = []
+
+  attr_accessor :name, :pets
   attr_reader :species
 
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @species = "human"
-    @@all << self
-    @pets = {fishes: [], cats: [], dogs: []}
-  end
-
   def self.all
-    @@all
+    OWNERS
   end
 
   def self.reset_all
-    @@all.clear
+    self.all.clear
   end
 
   def self.count
-    @@all.size
+    self.all.count
   end
 
-  def say_species
-    "I am a #{self.species}."
+  def initialize(species)
+    @species = species
+    OWNERS << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def buy_fish(name)
-    new_fish = Fish.new(name)
-    pets[:fishes] << new_fish
-  end
-
-  def buy_cat(name)
-    new_cat = Cat.new(name)
-    self.pets[:cats] << new_cat
+    pets[:fishes] << Fish.new(name)
   end
 
   def buy_dog(name)
-    new_dog = Dog.new(name)
-    self.pets[:dogs] << new_dog
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
   end
 
   def walk_dogs
-    pets[:dogs].each { |dog| dog.mood = 'happy' }
-  end
+      pets[:dogs].each { |dog| dog.mood ="happy" }
+    end
 
-  def play_with_cats
-    pets[:cats].each { |cat| cat.mood = 'happy' }
-  end
+    def play_with_cats
+      pets[:cats].each { |cat| cat.mood = "happy" }
+    end
 
-  def feed_fish
-    pets[:fishes].each { |fish| fish.mood = 'happy' }
-  end
+    def feed_fish
+      pets[:fishes].each { |fish| fish.mood = "happy" }
+    end
+
 
   def sell_pets
-    pets.each do |species, individuals|
-      individuals.each { |pet| pet.mood = 'nervous' }
-      pets[species] = []
+    pets.each do |animals, pet|
+      pet.each do |p|
+        p.mood = "nervous"
+      end
+      pet.clear
+    end
   end
-end
+
+  def say_species
+    "I am a #{species}."
+  end
 
   def list_pets
     "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
