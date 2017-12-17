9942eb18ef7f85280348a9f00e81f297831ecc74
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 20:40:57 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,13 @@
 class Fish
-  # code goes here
+  # can't change its name
+  attr_reader :name
+  # can change its mood
+  attr_accessor :mood
+
+  # initializes with a name
+  # initializes with a nervous mood
+  def initialize(name)
+    @name = name
+    @mood = 'nervous'
+  end
 end
