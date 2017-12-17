487085b00cd46db5be2587fa8a6caa0c380d8db8
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 20:50:23 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  # can't change its species
+  attr_reader :species
+
+  OWNERS = []
+
+  # keeps track of the owners that have been created
+  def self.all
+    OWNERS
+  end
+
+  # can count how many owners have been created
+  def self.count
+    OWNERS.size
+  end
+
+  # can reset the owners that have been created
+  def self.reset_all
+    OWNERS.clear
+  end
+
+  # can initialize an owner
+  # initializes with a species
+  def initialize (species)
+    @species = species
+  end
+
+  # can say its species
+  def say_species
+  end
 end
