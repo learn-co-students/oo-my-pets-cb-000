a75f6ae3813f2a2e0e2462384034c72f16f4a84a
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 20:54:54 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -22,9 +22,11 @@ class Owner
 
   # can initialize an owner
   # initializes with a species
+  # is initialized with a pets attribute as a hash with 3 keys
   def initialize (species)
     @species = species
     OWNERS << self
+    @pets = { :fishes=>[], :dogs=>[], :cats=>[] }
   end
 
   # can say its species
