dee3c2db67b3b6b904ce75e374273557e183ca29
marysue <mary@marylark.com>
2019-02-27 14:16:24 -0800

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood #owner object
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def play_with_cats
+    @mood = "happy"
+  end
 end
