cc83d8236431a140a40ce5873a8fefb0a14ef834
Kathleen Howard <kmaryhoward@gmail.com>
2019-05-14 20:41:29 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
+
 class Cat
-  # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name, mood="nervous")
+    @name = name
+    @mood = mood
+  end
+
+  private
+  def name=(name)
+    @name
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-  # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name, mood="nervous")
+    @name = name
+    @mood = mood
+  end
+
+  private
+  def name=(name)
+    @name
+  end
 end
